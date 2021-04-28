6dd99ae8653d38c16c874351b6bb109a5a92c789
g3andre <andre.mmp@gmail.com>
2021-04-28 12:12:58 -0300

    adicionado um contador

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const iPeso = document.querySelector("#peso");
 const iAltura = document.querySelector("#altura");
 const btnCalcular = document.querySelector("button");
 const dAlert = document.getElementById("alert");
+let timeToIncrement;
 
 (function inicio() {
     btnCalcular.addEventListener('click', btnCalcularOnClick);
+    counter();
 })()
 
 function btnCalcularOnClick() {
@@ -19,7 +21,7 @@ function btnCalcularOnClick() {
     showAlertMessage(message);
 }
 
-function showMessage(message) {    
+function showMessage(message) {
     dAlert.classList.replace('alert-danger', 'alert-warning')
     dAlert.innerHTML = message;
     dAlert.style.display = "block";
@@ -65,4 +67,74 @@ function hasInvalidateData() {
     else if (isNaN(iAltura.value) || iAltura.value.length == 0) message = `<strong>Altura Inválida...</strong>`;
 
     return message;
+}
+
+function counter() {
+    const timer = document.querySelector('#timer');
+    const timeUpdate = document.querySelector('#time-update');
+    const hour = document.querySelector('#hr');
+    const minute = document.querySelector('#min');
+    const second = document.querySelector('#sec');
+    const start = document.querySelector('#start');
+    const stop = document.querySelector('#stop');
+    const reset = document.querySelector('#reset');
+    timeToIncrement = new Date(60 * 60 * 3 * 1000);
+
+    let interval;
+    updateContent(timeToIncrement, timeUpdate);
+
+    start.addEventListener('click', () => {
+        if (!interval) {
+            interval = iniciaInterval(timeToIncrement, timeUpdate);
+            timer.classList.remove('text-danger');
+        }
+    });
+    stop.addEventListener('click', () => {
+        if (interval) {
+            interval ? clearInterval(interval) : null
+            interval = null;
+            updateContent(timeToIncrement, timeUpdate);
+            timer.classList.toggle('text-danger');
+        }
+    });
+    reset.addEventListener('click', () => {
+        timeToIncrement = new Date(60 * 60 * 3 * 1000);
+        interval ? clearInterval(interval) : null;
+        interval = null;
+        updateContent(timeToIncrement, timeUpdate);
+        timer.classList.remove('text-danger');
+        /* hour.innerText = '00';
+        minute.innerText = '00';
+        second.innerText = '00'; */
+    });
+
+}
+
+function iniciaInterval(date, element) {
+    return setInterval(() => {
+        date.setSeconds(date.getSeconds() + 1);
+        updateContent(timeToIncrement, element);
+    }, 1000);
+}
+
+function updateContent(value, element) {
+    element.innerText = value.toLocaleTimeString('pt-BR');
+}
+
+
+function initInterval(hour, minute, second) {
+    return setInterval(() => {
+        if (second.innerText == 59) {
+            second.innerText = '00';
+            minute.innerText = minute.innerText < 9 ? `0${minute.innerText*1+1}` : minute.innerText * 1 + 1;
+        } else {
+            second.innerText = second.innerText < 9 ? `0${second.innerText*1 + 1}` : second.innerText * 1 + 1;
+        }
+
+        if (minute.innerText == 60) {
+            minute.innerText = '00';
+            hour.innerText = hour.innerText < 9 ? `0${hour.innerText*1+1}` : hour.innerText * 1 + 1;
+        }
+
+    }, 1000);
 }
